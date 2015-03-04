a1b884f4e1b1b3671e92af8c11a4a83cfc397d82
scotttong <scotttong@sockmonster-2.local>
2015-03-03 22:09:14 -0800

    project setup
    one tab bar controller, 5 individual view controllers

---
+++ Tumblr/TabBarViewController.swift
@@ -0,0 +1,37 @@
+//
+//  TabBarViewController.swift
+//  Tumblr
+//
+//  Created by Scott Tong on 3/3/15.
+//  Copyright (c) 2015 Scott Tong. All rights reserved.
+//
+
+import UIKit
+
+class TabBarViewController: UIViewController {
+
+	@IBOutlet weak var contentView: UIView!
+	
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
